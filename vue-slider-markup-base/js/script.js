687f9390706d13c71c77af7a93ca882a52dd29ad
FrancescoBoco <francesco.bocoux@gmail.com>
2023-07-03 17:46:00 +0200

    /////////

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -27,41 +27,43 @@ console.log(createApp)
 createApp({
     data(){
         return{ 
+            activeSlide: 0,
             slides: [
             {
                 image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                    display: 'true'
                 }, {
                 image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    display: 'false'
                 }, {
                 image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    display: 'false'
                 }, {
                 image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    display: 'false'
                 }, {
                 image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    display: 'false'
                 }
-            ]
+            ],
+            
 
         }
     },
     methods: {
-        selectSlide(indexSlides){
-            if(this.splides[indexSlides].display == false){
-                this.splides[indexSlides].display == true;
+        selectSlide(){
+            activeSlide += 1
+
+            if (activeSlide == 6  ){
+                activeSlide = 1
+            if (activeSlide >= slides.length  ){
+                activeSlide = 0
+            }
             }
         }
     }
