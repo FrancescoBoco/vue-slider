27e8a77d929516a21ab18a2828b8753e6e0296a1
FrancescoBoco <francesco.bocoux@gmail.com>
2023-07-03 16:27:45 +0200

    ADD ciclo for

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -32,29 +32,38 @@ createApp({
                 image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    display: 'true'
                 }, {
-                    image: 'img/02.webp',
+                image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    display: 'false'
                 }, {
-                    image: 'img/03.webp',
+                image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    display: 'false'
                 }, {
-                    image: 'img/04.webp',
+                image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    display: 'false'
                 }, {
-                    image: 'img/05.webp',
+                image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    display: 'false'
                 }
             ]
 
         }
     },
     methods: {
-        
+        slectSlide(indexSlides){
+            if(this.splides[indexSlides].display == false){
+                this.splides[indexSlides].display == true;
+            }
+        }
     }
 }).mount('#app')
 
